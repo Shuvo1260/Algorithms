166423303e794b01350dfb26caf2002d213794a1
shovo <mdhassy753@gmail.com>
2019-09-19 22:20:25 +0600

    Coin Change algorithm bug fixed.

--- Dyanamic Programming/Coin Change/Coin Change.cpp
+++ Dyanamic Programming/Coin Change/Coin Change.cpp
@@ -2,26 +2,25 @@
 
 using namespace std;
 
+
+/// This function determines how much way we have to
+/// make the amount using given coins (1, 5, 10, 25, 50)
+
 int coinChange(int coins[], int totalCoin, int amount)
 {
+    /// Initializing the whole table with value 0
     int table[amount+1] = {0};
 
+    // First value set with 1
     table[0] = 1;
 
-    for(int coin = 0; coins[coin] <= amount; coin++)
+    for(int coin = 0; coin < totalCoin; coin++)
     {
         for(int index = coins[coin]; index <= amount; index++)
         {
             table[index] += table[index-coins[coin]];
         }
     }
-//    for(int coin = 0; coin < totalCoin; coin++)
-//    {
-//        for(int index = coins[coin]; index <= amount; index++)
-//        {
-//            table[index] += table[index-coins[coin]];
-//        }
-//    }
 
     return table[amount];
 
